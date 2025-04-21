e402b90bcc6c35df0a7a009df790ff5591e09f13
khghouse <khghouse@naver.com>
2025-04-21 09:55:47 +0900

    health-check 추가

---
+++ src/main/java/com/example/cicd/api/HealthCheckController.java
@@ -0,0 +1,17 @@
+package com.example.cicd.api;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health-check")
+public class HealthCheckController {
+
+    @GetMapping
+    public ResponseEntity<String> healthCheck() {
+        return ResponseEntity.ok("ok");
+    }
+
+}
