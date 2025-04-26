078e9eaf38280bb3d43adbb0b6077b7421a7568c
khghouse <khghouse@naver.com>
2025-04-26 23:45:32 +0900

    webhooks test

--- src/main/java/com/example/cicd/api/HealthCheckController.java
+++ src/main/java/com/example/cicd/api/HealthCheckController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/health-check")
 public class HealthCheckController {
 
+
     @GetMapping
     public ResponseEntity<String> healthCheck() {
         return ResponseEntity.ok("ok");
